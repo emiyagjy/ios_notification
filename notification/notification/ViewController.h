5d7378803d8e5f9821dafbf27e0594f7a4a55d80
GujyHy <gujyhy@GujyHydeMacBook-Air.local>
2015-08-31 13:32:56 +0800

    new data

---
+++ notification/notification/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  notification
+//
+//  Created by GujyHy on 15/8/31.
+//  Copyright (c) 2015年 GujyHy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ notification/notification/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  notification
+//
+//  Created by GujyHy on 15/8/31.
+//  Copyright (c) 2015年 GujyHy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
